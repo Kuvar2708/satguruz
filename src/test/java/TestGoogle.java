bd6dc2ed18aff6883ebf0f8b0cf4fb4b201d4d88
Kuvar2708 <fatehsingh4027@gmail.com>
2026-04-21 18:39:10 +0530

    Update TestGoogle.java

--- src/test/java/TestGoogle.java
+++ src/test/java/TestGoogle.java
@@ -1,8 +1,11 @@
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestGoogle {
-    public static void main(String[] args) {
+
+    @Test
+    public void openGoogle() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println("Title: " + driver.getTitle());
